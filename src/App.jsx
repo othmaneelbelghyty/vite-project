03f5db19ff92953a39f7938a76aaacae9ada5343
othmaneelbelghyty <othmane.elbelghyty@gmail.com>
2023-11-21 17:29:13 +0100

    vite-project

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <nav>
+          <a href="#hellooo">Home</a>
+          <a href="#medi1tv">News</a>
+          <a href="#ohtmane.elbelghyty@gmail.com">Contact</a>
+          <a href="#othmane.elbelghyty">About</a>
+        </nav>
+        <div class="container">
+          <div class="column">
+            <img src="IMG-0473.jpg" alt="IMG-0473.jpg" />
+            <h1>Colton Smith</h1>
+            <h2>Verified Buyer</h2>
+            <p>
+              "We needed the same printed design as the one we had ordered a
+              week prior.Not only did they find the original order,but we also
+              received it in time. Excellent!"
+            </p>
+          </div>
+          <div class="column">
+            <img src="IMG-0471.jpg" alt="IMG-0471.jpg" />
+            <h1>Irene Roberts</h1>
+            <h2>Verified Buyer</h2>
+            <p>
+              "Customer service is always excellent and very quick turn around.
+              Completely delighted with the simplicity of the purchase and the
+              speed of delivery."
+            </p>
+          </div>
+          <div class="column">
+            <img src="IMG-0472.jpg" alt="IMG-0472.jpg" />
+            <h1>Anne Wallace</h1>
+            <h2>Verified Buyer</h2>
+            <p>
+              "Put an order with this company and can only prais them for the
+              ver high standard.Will definitely use them again and recommendthem
+              to everyone!"
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
