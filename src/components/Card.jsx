9c42db3f8feb07a504597a65d0f1339d327ab3e0
othmaneelbelghyty <othmane.elbelghyty@gmail.com>
2023-11-22 10:44:44 +0100

    vite-projecte

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+const Card = () => {
+  return (
+    <div>
+      <div class="column">
+        <img src="IMG-0473.jpg" alt="IMG-0473.jpg" />
+        <h1>Colton Smith</h1>
+        <h2>Verified Buyer</h2>
+        <p>
+          "We needed the same printed design as the one we had ordered a week
+          prior.Not only did they find the original order,but we also received
+          it in time. Excellent!"
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Card;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <a href="#hellooo">Home</a>
+        <a href="#medi1tv">News</a>
+        <a href="#ohtmane.elbelghyty@gmail.com">Contact</a>
+        <a href="#othmane.elbelghyty">About</a>
+      </nav>
+    </div>
+  );
+};
+export default NavBar;
